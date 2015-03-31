571d6bdf3d893dcdcf2c222f6d0dd0056b3db576
leiha <leiha.sellier@gmail.com>
2015-03-31 13:38:48 +0200

    add test for PDO update query

--- src/Test/Database/PdoBundle/Controller/UpdateController.php
+++ src/Test/Database/PdoBundle/Controller/UpdateController.php
@@ -3,12 +3,24 @@
 namespace Test\Database\PdoBundle\Controller;
 
 use Lia\KernelBundle\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class UpdateController
     extends ControllerBase
 {
     public function indexAction()
     {
+        $pdo = $this->get('lia.factory.database.pdo')->getConnection();
 
+        $q = $pdo->updateTable('test')
+            ->set('name=:name')
+            //->whereInline('id=:id')
+            ->where()
+                ->equal('id','toto')
+            ->end()
+            ->getQuery(['name'=> 'toto', 'toto'=> [14,12]]);
+
+        return new Response($q);
     }
 }
